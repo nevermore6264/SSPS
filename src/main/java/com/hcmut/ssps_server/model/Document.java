b5dc5e4594510738bff807b1656d0143c38ae09e
Triet28 <duetriet2806@gmail.com>
2024-10-25 14:18:55 +0700

    model Document và Printer

--- src/main/java/com/hcmut/ssps_server/model/Document.java
+++ src/main/java/com/hcmut/ssps_server/model/Document.java
@@ -1,4 +1,37 @@
 package com.hcmut.ssps_server.model;
 
+import com.hcmut.ssps_server.model.user.Student;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long docId;
+
+    @Column(nullable = false, unique = true)
+    Long studentUploadID;
+
+    @Column(nullable = false)
+    String fileName;
+
+    @Column(nullable = false)
+    String fileType;
+
+    @Column(nullable = false)
+    double fileSize;
+
+    @Column(nullable = false)
+    int pageCount;
+
+    @ManyToOne
+    @JoinColumn(name = "studentUploadID", referencedColumnName = "studentId", insertable = false, updatable = false)
+    Student student;
 }
--- src/main/java/com/hcmut/ssps_server/model/Printer.java
+++ src/main/java/com/hcmut/ssps_server/model/Printer.java
@@ -1,4 +1,39 @@
 package com.hcmut.ssps_server.model;
 
+import java.util.List;
+import java.util.Queue;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Printer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long printerID;
+
+    @Column(nullable = false)
+    String printerLocation;
+
+    @Column(nullable = false)
+    boolean printerStatus;
+
+    @Column(nullable = false)
+    @Min(0)
+    int papersLeft;
+
+    @ElementCollection
+    @CollectionTable(name = "available_doc_types", joinColumns = @JoinColumn(name = "printer_id"))
+    @Column(name = "doc_type")
+    List<String> availableDocType;
+
+    @ElementCollection
+    @CollectionTable(name = "student_queue", joinColumns = @JoinColumn(name = "printer_id"))
+    @Column(name = "student_id")
+    Queue<Integer> studentIDQueue;
 }
